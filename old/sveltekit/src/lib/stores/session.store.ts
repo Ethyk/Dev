25e04d855872a27dca1fdd386b0e86d91aeb4ae1
Ethyk <admin@devcraft.us>
2025-04-15 21:55:30 +0200

    clean

--- old/sveltekit/src/lib/stores/session.store.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const session = writable<{
-    user: null | any;
-    loading: boolean;
-}>({
-    user: null,
-    loading: true
-});
--- old/sveltekit/src/routes/(app)/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async ({ locals }) => {
-    console.log("user : ", locals.user);
-    return {
-        user: locals.user || null
-    };
-}) satisfies LayoutServerLoad;
